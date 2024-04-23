a19419f4f15550ddfafc3b96880ec5b352ccb879
paalwilliams-aaron-ai <158501128+paalwilliams-aaron-ai@users.noreply.github.com>
2024-04-23 20:58:11 +0200

    move website to top level

--- svelte.config.ts
+++ svelte.config.ts
@@ -8,6 +8,10 @@ const config: Config = {
 	// for more information about preprocessors
 	preprocess: vitePreprocess(),
 
+	paths: {
+		base: import.meta.env.NODE_ENV === 'production' ? '/sveltekit-github-pages' : ''
+	},
+
 	kit: {
 		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
 		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
