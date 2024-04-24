b75b0af5708afa01953eecd863d9d82aedaca036
paalwilliams-aaron-ai <158501128+paalwilliams-aaron-ai@users.noreply.github.com>
2024-04-24 09:08:27 +0200

    add build script and main page

--- src/lib/components/Home.svelte
+++ src/lib/components/Home.svelte
@@ -1,5 +1,7 @@
 <section class="h-screen flex items-center justify-center flex-col">
 	<p class="text-center">Paal Williams</p>
-	<p class="text-center underline hover:italic"><a href="mailto:hello@paaa.al">hello@paaa.al</a></p>
+	<p class="text-center underline hover:italic">
+		<a href="mailto:paalwilliams@gmail.com" target="_top">paalwilliams@gmail.com</a>
+	</p>
 	<p class="text-center">berlin</p>
 </section>
