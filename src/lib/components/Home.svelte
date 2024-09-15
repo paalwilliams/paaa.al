0723dc3a66b381087cb9e63dce498edc1bc87622
Paal <paal@Paals-MacBook-Pro-3.local>
2024-09-15 18:39:35 +0200

    update home page

--- src/lib/components/Home.svelte
+++ src/lib/components/Home.svelte
@@ -1,7 +1,8 @@
 <section class="h-screen flex items-center justify-center flex-col">
 	<p class="text-center">Paal Williams</p>
+	<p class="text-center">Software Engineer @ Doctolib</p>
 	<p class="text-center underline hover:italic">
 		<a href="mailto:paalwilliams@gmail.com" target="_top">paalwilliams@gmail.com</a>
 	</p>
-	<p class="text-center">berlin</p>
+	<p class="text-center">Berlin, DE</p>
 </section>
